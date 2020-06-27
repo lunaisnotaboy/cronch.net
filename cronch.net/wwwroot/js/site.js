8e91bd15c58cc7aa79cfce81b40df1e18a2ea283
lunaisnotaboy <stampylongr7@gmail.com>
2020-06-27 00:03:48 -0400

    general tomfoolery

--- cronch.net/wwwroot/js/site.js
+++ cronch.net/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const source = 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
+    const video = document.querySelector('video'); // For options (second argument) see: https://github.com/sampotts/plyr/#options
+
+    const player = new Plyr(video); // Expose player so it can be used from the console
+
+    window.player = player; // For more Shaka Player options, see: https://github.com/google/shaka-player
+
+    if (shaka.Player.isBrowserSupported()) {
+        // Install built-in polyfills
+        shaka.polyfill.installAll();
+        const shakaInstance = new shaka.Player(video);
+        shakaInstance.load(source);
+    } else {
+        console.warn('Browser is not supported!');
+    }
+});
